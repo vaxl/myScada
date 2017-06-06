e02ad27ca059de4b320d38cbd92720f2a1113409
Vaxl <vaxl@mail.ru>
2017-06-06 23:39:01 +0300

    migrate to gradle

--- myScada2/src/main/java/Main.java
+++ myScada2/src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         ApplicationContext contex = new ClassPathXmlApplicationContext("spring.xml");
         View view = contex.getBean("consoleView", View.class);
         MainController controller = contex.getBean(MainController.class);
+        System.out.println("start application");
         controller.start(view);
 
     }
